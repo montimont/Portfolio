ec0d2c2a7906db1b4092c7435b43da91300cf490
montimont <123782959+montimont@users.noreply.github.com>
2023-06-28 22:08:48 -0400

    Update stock.py

--- Machine Learing/Stock Prediction/stock.py
+++ Machine Learing/Stock Prediction/stock.py
@@ -8,13 +8,11 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 
-# load data
 company = 'PDD'
 start = dt.datetime(2012,1,1)
 end = dt.datetime(2020,1,1)
 data = yf.download(company, start, end)
 
-# prepare data
 scaler = MinMaxScaler(feature_range=(0,1))
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
 prediction_days = 60
@@ -26,7 +24,6 @@ for x in range(prediction_days, len(scaled_data)):
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-# build the model
 model = Sequential()
 model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
 model.add(Dropout(0.2))
@@ -38,7 +35,6 @@ model.add(Dense(units=1))
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=250, batch_size=32)
 
-# test the model accuracy on existing data
 test_start = dt.datetime(2020,1,1)
 test_end = dt.datetime.now()
 test_data = yf.download(company, test_start, test_end)
@@ -55,7 +51,6 @@ x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 predicted_prices = model.predict(x_test)
 predicted_prices = scaler.inverse_transform(predicted_prices)
 
-# plot the test predictions
 plt.plot(actual_prices, color='black', label=f"Actual {company} Price")
 plt.plot(predicted_prices, color='green', label=f"Predicted {company} price")
 plt.title(f"{company} share price")
@@ -64,7 +59,6 @@ plt.ylabel(f"{company} share price")
 plt.legend()
 plt.show()
 
-# predict next day
 real_data = [model_inputs[len(model_inputs) + 1 - prediction_days:len(model_inputs)]]
 real_data = np.array(real_data)
 real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
